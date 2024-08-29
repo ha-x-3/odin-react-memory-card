7e5ce6cba9ffdcc127233599c0bbe22161abab46
Brittney Epperson <beppers87@hotmail.com>
2024-08-29 14:15:14 -0500

    Added background, title, and favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<>
+			<div></div>
+			<small>
+				Photo by{' '}
+				<a href='https://unsplash.com/@alvannee?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash'>
+					Alvan Nee
+				</a>{' '}
+				on{' '}
+				<a href='https://unsplash.com/photos/white-and-brown-long-fur-cat-ZCHj_2lJP00?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash'>
+					Unsplash
+				</a>
+				<a
+					target='_blank'
+					href='https://icons8.com/icon/36886/maneki'
+				>
+					&nbsp; &#x2022; Maneki
+				</a>{' '}
+				icon by{' '}
+				<a
+					target='_blank'
+					href='https://icons8.com'
+				>
+					Icons8
+				</a>
+			</small>
+		</>
+	);
 }
 
-export default App
+export default App;
